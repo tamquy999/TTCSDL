42dd7b393c57d33682c5f2335da566e050d87e87
Qynklee <anhtu16101999@gmail.com>
2020-05-12 16:45:48 +0700

    Thêm hash password trước khi login. Sửa DB

--- DAO/DBProvider.cs
+++ DAO/DBProvider.cs
@@ -4,35 +4,61 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAO
 {
-    public class DBProvider
-    {
-        protected SqlConnection _conn = new SqlConnection(@"Data Source=DESKTOP-NATE\SQLEXPRESS;Initial Catalog=TTCSDL;Integrated Security=True");
-     
-        public bool Login(string user, string pass)
-        {
-            string query = "SELECT * FROM THUNGAN WHERE MATHUNGAN = '" + user + "' AND PASSWORD = '" + pass + "'";
-            SqlDataAdapter da = new SqlDataAdapter(query, _conn);
-            
-            DataTable dt = new DataTable();
+     public class DBProvider
+     {
+          protected SqlConnection _conn = new SqlConnection(@"Data Source=DESKTOP-FIFGC10\SQLEXPRESS;Initial Catalog=TTCSDLv81;Integrated Security=True");
 
-            try
-            {
-                da.Fill(dt);
-                if (dt.Rows.Count > 0)
-                {
-                    return true;
-                }
-            }
-            catch (Exception)
-            {
-            }
-            return false;
+          public bool Login(string user, string pass)
+          {
+               string hashedPass = HashPass(pass);
 
-        }
-    }
+               
+               string query = "SELECT * FROM THUNGAN WHERE MATHUNGAN = '" + user + "' AND PASSWORD = '" + hashedPass + "'";
+               SqlDataAdapter da = new SqlDataAdapter(query, _conn);
+
+               DataTable dt = new DataTable();
+
+               try
+               {
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                         return true;
+                    }
+               }
+               catch (Exception)
+               {
+               }
+               return false;
+
+          }
+          #region Hashing Pass
+          public string HashPass(string pass)
+          {
+               SHA1 shaH = new SHA1CryptoServiceProvider();
+               MD5 md5H = new MD5CryptoServiceProvider();
+
+               byte[] step1 = md5H.ComputeHash(Encoding.ASCII.GetBytes(pass));
+               string strs1 = ByteArrayToString(step1);
+               byte[] step2 = shaH.ComputeHash(Encoding.ASCII.GetBytes(strs1));
+
+               string hashedPass = ByteArrayToString(step2);
+               return hashedPass;
+          }
+
+          public static string ByteArrayToString(byte[] ba)
+          {
+               StringBuilder hex = new StringBuilder(ba.Length * 2);
+               foreach (byte b in ba)
+                    hex.AppendFormat("{0:x2}", b);
+               return hex.ToString();
+          }
+          #endregion
+     }
 }
