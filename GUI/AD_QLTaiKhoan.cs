71cc7f3db94c14f4b4035eec9835116997fa1025
Qynklee <anhtu16101999@gmail.com>
2021-01-04 13:40:36 +0700

    them ad_qlnhanvien

---
+++ GUI/AD_QLTaiKhoan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace GUI
+{
+    public partial class AD_QLTaiKhoan : DevExpress.XtraEditors.XtraUserControl
+    {
+        public AD_QLTaiKhoan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTraCuu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
